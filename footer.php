0eb1cd7407e3651a56d0f938ecbebd437def2dae
James Hager <jimbodaman12@gmail.com>
2020-04-02 15:17:28 -0600

    renamed pages to php, included footer and header

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <p class="float-right">
+      <a href="#">Back to top</a>
+    </p>
+    <br>
+    <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+    
+  </div>
---
+++ index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.6">
+    <title>James Hager Portfolio</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/album/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <header>
+    <?php require_once('header.php');?>
+    </header>
+    
+<main role="main">
+
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1>James Hager's Portfolio</h1>
+      <p class="lead text-muted">Projects and Work done throughout my time in the GIMM program.</p>
+      <p>
+        <a href="#" class="btn btn-primary my-2">View all Projects</a>
+        <a href="Contact.html" class="btn btn-secondary my-2">Contact Info</a>
+      </p>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img class ="card-img-top" src= "TOZ_thumb.PNG">
+            <div class="card-body">
+              <h5 class="card-title">Tomb of Azothin</h5>
+              <p class="card-text">First group coding project done within GIMM 110. Created a scrolling turn based game from scratch in Adobe Animate. Later, I created a trailer for this game in GIMM 270.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group align-items-center">
+                  <a href="Tomb of Azothin.html" class="btn btn-sm btn-outline-secondary" role="button">View Project Page</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img class="card-img-top" src="TOZ_thumb.PNG">
+            <div class="card-body">
+              <h5 class="card-title">360 Video Project</h5>
+              <p class="card-text">First VR project that utilized Unity and 360 video.  Created a thriller scenario with an interactive element to show 2 different sides of the story.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="360 Video Project.html" class="btn btn-sm btn-outline-secondary" role="button">View Project Page</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img class="card-img-top" src="TOZ_thumb.PNG">
+            <div class="card-body">
+              <h5 class="card-title">STEM Essentials App</h5>
+              <p class="card-text">App designed to help someone learn some essentials regarding STEM concepts.  Perfect for people that don't know anything about STEM and just want to learn a little more or be refreshed on concepts.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="STEMApp.html" class="btn btn-sm btn-outline-secondary" role="button">View Project Page</a>
+                </div>       
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<footer class="text-muted">
+  <?php require_once('footer.php');?>
+</footer>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</html>
